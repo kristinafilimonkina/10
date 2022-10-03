98e05fd96a6aab0f8ea24cfd9a92e237c623a1d0
kristinafilimonkina <97752607+kristinafilimonkina@users.noreply.github.com>
2022-10-03 18:55:25 +0400

    init commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,17 +1,50 @@
 package com.company;
 
-import java.util.Scanner;
-
 public class Main {
-
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Введите ваше предложение:");
-        String words = scan.nextLine();
-        String[] transwords = words.split("[\\d\\s]+");
-        System.out.print("Кол-во слов: " + transwords.length);
+        AnimalThread thread1 = (new AnimalThread("Черепаха", 10));
+        AnimalThread thread2 = (new AnimalThread("Кролик", 1));
+        thread1.start();
+        thread2.start();
+    }
+}
+class AnimalThread extends Thread {
+    int priority;
+    String Name;
+    final int distance = 11;
 
+    public AnimalThread(String Name, int priority) {
+        this.Name = Name;
+        this.priority = priority;
+    }
 
+    @Override
+    public void run() {
+
+        Thread.currentThread().setPriority(priority);
+        Thread.currentThread().setName(Name);
+        int timeToStep = 1000 / (Thread.currentThread().getPriority());
+
+        for (int i = 0; i < distance; i++) {
+            try {
+                Thread.sleep(timeToStep);
+                System.out.println(getName() + (" - ") + (i * 100) + " м ");
+            } catch (InterruptedException e) {
+
+                e.printStackTrace();
+            }
+            if (i == distance / 2) {
+
+                if (Thread.currentThread().getPriority() == 1) {
+                    Thread.currentThread().setPriority(10);
+                } else {
+                    Thread.currentThread().setPriority(1);
+                }
+                timeToStep = 1000 / (Thread.currentThread().getPriority());
+            }
+            if (i == distance - 10) {
+            }
+        }
 
     }
 }
